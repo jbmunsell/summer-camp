66be50bbb7115b95d215d8461daf0b83c0d90214
jbmunsell <41763534+jbmunsell@users.noreply.github.com>
2020-10-30 19:05:04 -0500

    Changed schedule notification text

--- src/schedule/schedule.config.lua
+++ src/schedule/schedule.config.lua
@@ -1,9 +1,12 @@
 local StartMessages = {
 	LightsOut = "Lights out! Head to your cabins and go to bed for the night.",
-	FreeTime = "Free time! Explore, play, relax, or do whatever you like.",
 	MorningRun = "Rise and shine! Follow your cabin leaders on a morning run around the lake.",
+
 	Meal = "Meal time! Follow your cabin leaders to the dining area.",
-	Activity = "Activity time! Follow your cabin leaders to an activity.",
+	MealOver = "Meal time is over! Drop your trays and dishes off at the cleaning tent and ",
+
+	FreeTime = "enjoy your free time.",
+	Activity = "follow your cabin leaders to an activity.",
 }
 
 local scheduleConfig = {
@@ -15,15 +18,64 @@ local scheduleConfig = {
 	TimeScaleTweenInfo = TweenInfo.new(2, Enum.EasingStyle.Cubic, Enum.EasingDirection.InOut),
 	
 	agenda = {
-		{ Name = "LightsOut", Duration = 8.25, StartingTime = 0, DisplayName = "Lights Out", StartMessage = StartMessages.LightsOut },
-		{ Name = "MorningRun", Duration = 0.25, DisplayName = "Morning Run", StartMessage = StartMessages.MorningRun },
-		{ Name = "Breakfast", Duration = 0.5, DisplayName = "Breakfast", MealKey = "breakfast", StartMessage = StartMessages.Meal },
-		{ Name = "OpenActivityChunk", Duration = 4.0, DisplayName = "Morning Activity", StartMessage = StartMessages.Activity },
-		{ Name = "Lunch", Duration = 0.5, DisplayName = "Lunch", MealKey = "lunch", StartMessage = StartMessages.Meal },
-		{ Name = "OpenActivityChunk", Duration = 4.0, DisplayName = "Afternoon Activity", StartMessage = StartMessages.Activity },
-		{ Name = "Dinner", Duration = 0.5, DisplayName = "Dinner", MealKey = "dinner", StartMessage = StartMessages.Meal },
-		{ Name = "FreeTime", Duration = 4.0, DisplayName = "Free Time", StartMessage = StartMessages.FreeTime },
-		{ Name = "LightsOut", Duration = 2, DisplayName = "Lights Out", StartMessage = StartMessages.LightsOut },
+		{
+			Name = "LightsOut",
+			Duration = 8.25,
+			StartingTime = 0,
+			DisplayName = "Lights Out",
+			StartMessage = StartMessages.LightsOut
+		},
+		{
+			Name = "MorningRun",
+			Duration = 0.25,
+			DisplayName = "Morning Run",
+			StartMessage = StartMessages.MorningRun
+		},
+		{
+			Name = "Breakfast",
+			Duration = 0.5,
+			DisplayName = "Breakfast",
+			MealKey = "breakfast",
+			StartMessage = StartMessages.Meal
+		},
+		{
+			Name = "OpenActivityChunk",
+			Duration = 4.0,
+			DisplayName = "Morning Activity",
+			StartMessage = StartMessages.MealOver .. StartMessages.Activity,
+		},
+		{
+			Name = "Lunch",
+			Duration = 0.5,
+			DisplayName = "Lunch",
+			MealKey = "lunch",
+			StartMessage = StartMessages.Meal
+		},
+		{
+			Name = "OpenActivityChunk",
+			Duration = 4.0,
+			DisplayName = "Afternoon Activity",
+			StartMessage = StartMessages.MealOver .. StartMessages.Activity
+		},
+		{
+			Name = "Dinner",
+			Duration = 0.5,
+			DisplayName = "Dinner",
+			MealKey = "dinner",
+			StartMessage = StartMessages.Meal
+		},
+		{
+			Name = "FreeTime",
+			Duration = 4.0,
+			DisplayName = "Free Time",
+			StartMessage = StartMessages.MealOver .. StartMessages.FreeTime
+		},
+		{
+			Name = "LightsOut",
+			Duration = 2,
+			DisplayName = "Lights Out",
+			StartMessage = StartMessages.LightsOut
+		},
 	}
 }
 
