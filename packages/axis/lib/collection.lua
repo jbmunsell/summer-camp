ea30f8d4b583136fa876b6f1370928aeb1ffd443
jbmunsell <41763534+jbmunsell@users.noreply.github.com>
2020-11-10 13:16:05 -0600

    Added axis collection lib

---
+++ packages/axis/lib/collection.lua
@@ -0,0 +1,45 @@
+--
+--	Jackson Munsell
+--	09 Nov 2020
+--	collection.lua
+--
+--	Utility module for folders that represent array-style data
+--
+
+-- modules
+local axis = script.Parent.Parent
+local rx = require(axis.lib.rx)
+local dart = require(axis.lib.dart)
+local tableau = require(axis.lib.tableau)
+
+-- lib
+local collection = {}
+
+-- add entry
+function collection.addValue(folder, entry)
+	local value = Instance.new(tableau.getValueObjectType(entry))
+	value.Name = tostring(#folder:GetChildren() + 1)
+	value.Value = entry
+	value.Parent = folder
+end
+
+-- get entry
+function collection.getValue(folder, value)
+	return tableau.from(folder:GetChildren())
+		:first(function (v) return v.Value == value end)
+end
+
+-- clear
+function collection.clear(folder)
+	folder:ClearAllChildren()
+end
+
+-- observe values changed
+function collection.observeChanged(folder)
+	return rx.Observable.from(folder.ChildAdded)
+		:merge(rx.Observable.from(folder.ChildRemoved))
+		:map(dart.constant(nil))
+end
+
+-- return lib
+return collection
