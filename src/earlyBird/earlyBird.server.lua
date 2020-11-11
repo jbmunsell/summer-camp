7576281a9ec64961403ab46815ee69c15088866f
jbmunsell <41763534+jbmunsell@users.noreply.github.com>
2020-11-11 12:05:44 -0600

    Added early bird badge

---
+++ src/earlyBird/earlyBird.server.lua
@@ -0,0 +1,43 @@
+--
+--	Jackson Munsell
+--	11 Nov 2020
+--	earlyBird.server.lua
+--
+--	Early bird badge server driver
+--
+
+-- env
+local Players = game:GetService("Players")
+local BadgeService = game:GetService("BadgeService")
+local env = require(game:GetService("ReplicatedStorage").src.env)
+local axis = env.packages.axis
+
+-- modules
+local rx = require(axis.lib.rx)
+local dart = require(axis.lib.dart)
+
+---------------------------------------------------------------------------------------------------
+-- Variables
+---------------------------------------------------------------------------------------------------
+
+local BadgeId = 0
+
+---------------------------------------------------------------------------------------------------
+-- Functions
+---------------------------------------------------------------------------------------------------
+
+-- Award badge
+local function awardBadge(player)
+	BadgeService:AwardBadge(player.UserId, BadgeId)
+end
+
+---------------------------------------------------------------------------------------------------
+-- Streams
+---------------------------------------------------------------------------------------------------
+
+-- Award to player on entered
+rx.Observable.from(Players.PlayerAdded)
+	:startWithTable(Players:GetPlayers())
+	:map(dart.carry(awardBadge))
+	:map(dart.bind)
+	:subscribe(spawn)
