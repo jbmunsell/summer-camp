1cd8ac0b011deb1f2d39ef3975bc3479140d9dea
jbmunsell <41763534+jbmunsell@users.noreply.github.com>
2020-11-14 13:12:43 -0600

    Added activity chunk gui display

--- src/gui/core/clock.client.lua
+++ src/gui/core/clock.client.lua
@@ -13,7 +13,9 @@ local schedule = env.src.schedule
 
 -- modules
 local rx = require(axis.lib.rx)
+local dart = require(axis.lib.dart)
 local scheduleUtil = require(schedule.util)
+local scheduleStreams = require(schedule.streams)
 
 -- instances
 local ClockFrame = env.PlayerGui:WaitForChild("Core").Container.Clock
@@ -23,7 +25,16 @@ local function setClockDisplay(timeString)
 	ClockFrame.TextLabel.Text = timeString
 end
 
+-- Set activity text
+local function setActivityText(text)
+	ClockFrame.Activity.TextLabel.Text = text
+end
+
 -- Change clock
 rx.Observable.from(schedule.interface.GameTimeHours)
 	:map(scheduleUtil.getTimeString)
 	:subscribe(setClockDisplay)
+
+-- Change schedule chunk
+scheduleStreams.scheduleChunk:map(dart.index("DisplayName"))
+	:subscribe(setActivityText)
