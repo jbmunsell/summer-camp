85ca26528288aa784201a7c2a1c5ee97cd9f195e
jbmunsell <41763534+jbmunsell@users.noreply.github.com>
2020-11-20 11:43:55 -0600

    Added autorun

---
+++ src/autorun/autorun.client.lua
@@ -0,0 +1,71 @@
+--
+--	Jackson Munsell
+--	20 Nov 2020
+--	autorun.client.lua
+--
+--	Auto runs the character after moving for a few seconds
+--
+
+-- env
+local TweenService = game:GetService("TweenService")
+local env = require(game:GetService("ReplicatedStorage").src.env)
+local axis = env.packages.axis
+
+-- modules
+local rx = require(axis.lib.rx)
+local dart = require(axis.lib.dart)
+local axisUtil = require(axis.lib.axisUtil)
+
+---------------------------------------------------------------------------------------------------
+-- Variables
+---------------------------------------------------------------------------------------------------
+
+local runningChanged = env.src.autorun.net.RunningChanged
+
+local RunningFOV = 80
+local WalkingFOV = 70
+local RunningSpeed = 32
+local WalkingSpeed = 20
+local AutorunTimer = 3
+local SpeedTweenInfo = TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut)
+
+---------------------------------------------------------------------------------------------------
+-- Functions
+---------------------------------------------------------------------------------------------------
+
+local function setRunning(running)
+	local humanoid = axisUtil.getLocalHumanoid()
+	local camera = workspace.CurrentCamera
+	local humanoidGoals = { WalkSpeed = running and RunningSpeed or WalkingSpeed }
+	local cameraGoals = { FieldOfView = running and RunningFOV or WalkingFOV }
+	TweenService:Create(humanoid, SpeedTweenInfo, humanoidGoals):Play()
+	TweenService:Create(camera, SpeedTweenInfo, cameraGoals):Play()
+	runningChanged:FireServer(running)
+end
+
+---------------------------------------------------------------------------------------------------
+-- Streams
+---------------------------------------------------------------------------------------------------
+
+local running = rx.Observable.from(env.LocalPlayer.CharacterAdded)
+	:startWith(env.LocalPlayer.Character)
+	:filter()
+	:map(function (character)
+		return character:WaitForChild("Humanoid")
+	end)
+	:filter()
+	:flatMap(function (humanoid)
+		return rx.Observable.fromInstanceEvent(humanoid, "Running")
+	end)
+	:map(dart.greaterThan(0.02))
+	:distinctUntilChanged()
+	:share()
+running:filter():subscribe(function ()
+	-- Speed them up after a few seconds if we're still running
+	local stopped = running:reject()
+	local fast = rx.Observable.timer(AutorunTimer):takeUntil(stopped)
+	local slow = stopped:first()
+	fast:map(dart.constant(true))
+		:merge(slow:map(dart.constant(false)))
+		:subscribe(setRunning)
+end)
---
+++ src/autorun/autorun.server.lua
@@ -0,0 +1,65 @@
+--
+--	Jackson Munsell
+--	20 Nov 2020
+--	autorun.server.lua
+--
+--	Turns on particles when client says they're running
+--
+
+-- env
+local env = require(game:GetService("ReplicatedStorage").src.env)
+local axis = env.packages.axis
+
+-- modules
+local fx = require(axis.lib.fx)
+local rx = require(axis.lib.rx)
+local dart = require(axis.lib.dart)
+local axisUtil = require(axis.lib.axisUtil)
+
+---------------------------------------------------------------------------------------------------
+-- Functions
+---------------------------------------------------------------------------------------------------
+
+local function renderCharacterRuning(character, running)
+	local emitter = character:FindFirstChild("SprintEmitter")
+	if emitter then
+		fx.setFXEnabled(emitter, running)
+	end
+end
+
+local function initCharacter(character)
+	local humanoid = character:FindFirstChild("Humanoid")
+	local root = character:FindFirstChild("HumanoidRootPart")
+
+	local emitter = env.res.character.SprintEmitter:Clone()
+	emitter.Parent = character
+	renderCharacterRuning(character, false)
+
+	local weld = Instance.new("Weld")
+	weld.Part0 = character.HumanoidRootPart
+	weld.Part1 = emitter
+
+	local function adjustWeld()
+		weld.C0 = CFrame.new(0, -0.5 * (root.Size.Y + humanoid.HipHeight), root.Size.Z * 0.5)
+			* CFrame.Angles(math.pi * 0.5, 0, 0)
+	end
+
+	rx.Observable.fromProperty(humanoid, "HipHeight", true)
+		:merge(rx.Observable.fromProperty(root, "Size"))
+		:subscribe(adjustWeld)
+
+	weld.Parent = emitter
+end
+
+---------------------------------------------------------------------------------------------------
+-- Streams
+---------------------------------------------------------------------------------------------------
+
+rx.Observable.from(env.src.autorun.net.RunningChanged)
+	:map(function (player, running)
+		return player.Character, running
+	end)
+	:filter()
+	:subscribe(renderCharacterRuning)
+
+axisUtil.getPlayerCharacterStream():map(dart.select(2)):subscribe(initCharacter)
