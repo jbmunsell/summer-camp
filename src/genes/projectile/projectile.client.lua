d51d16ee73451e7f1b14f9e91b2800918a8b6f78
jbmunsell <41763534+jbmunsell@users.noreply.github.com>
2020-12-16 10:49:17 -0600

    Added projectile charge and throw animations

--- src/genes/projectile/projectile.client.lua
+++ src/genes/projectile/projectile.client.lua
@@ -20,12 +20,68 @@ local genesUtil = require(genes.util)
 local pickupUtil = require(genes.pickup.util)
 local projectileUtil = require(genes.projectile.util)
 
+local projectileAnimations = env.res.genes.projectile.animations
+
+---------------------------------------------------------------------------------------------------
+-- Functions
+---------------------------------------------------------------------------------------------------
+
+local function playChargeAnimation(instance)
+	local animator = axisUtil.getLocalAnimator()
+	if not animator then return end
+	local anim = env.res.genes.projectile.animations.ChargeThrow
+	local chargeTime = instance.config.projectile.chargeTime.Value
+	local track = animator:LoadAnimation(anim)
+	while track.Length == 0 do wait() end -- Prevent nans from not fully loaded anims
+	rx.Observable.from(track:GetMarkerReachedSignal("FullChargeReached"))
+		:first()
+		:takeUntil(rx.Observable.from(track.Stopped))
+		:subscribe(function ()
+			animator:LoadAnimation(projectileAnimations.FullChargeHold):Play()
+		end)
+	track:Play(nil, nil, track.Length / chargeTime)
+end
+
+local function throw(instance, chargeProportion, target)
+	-- Variables
+	local config = instance.config.projectile
+	local start = axisUtil.getPosition(instance)
+	local vx = math.clamp(chargeProportion, 0, 1)
+	local velocity = config.maxThrowVelocity.Value * vx + config.minThrowVelocity.Value * (1 - vx)
+
+	-- Projectile firing
+	local function fireProjectile()
+		pickupUtil.stripObject(instance)
+		projectileUtil.fireOwnedProjectile(instance, start, target, velocity)
+		genes.projectile.net.ReleaseRequested:FireServer(instance, start, target, velocity)
+	end
+
+	-- Stop charge anim and play throw anim
+	local animator = axisUtil.getLocalAnimator()
+	if not animator then return end
+	local throwTrack = animator:LoadAnimation(projectileAnimations.Throw)
+	rx.Observable.from(throwTrack:GetMarkerReachedSignal("Throw"))
+		:first()
+		:takeUntil(rx.Observable.from(throwTrack.Stopped))
+		:subscribe(fireProjectile)
+	throwTrack:Play()
+	axisUtil.stopAnimationTrack(animator, projectileAnimations.ChargeThrow)
+	axisUtil.stopAnimationTrack(animator, projectileAnimations.FullChargeHold)
+end
+
 ---------------------------------------------------------------------------------------------------
 -- Streams
 ---------------------------------------------------------------------------------------------------
 
 -- init gene
 local projectiles = genesUtil.initGene(genes.projectile)
+
+-- Play charge animation according to projectile charging
+pickupUtil.getLocalCharacterHoldingStream(genes.projectile):switchMap(function (instance)
+	return instance
+	and rx.Observable.from(instance.state.projectile.charging):filter():map(dart.constant(instance))
+	or rx.Observable.never()
+end):subscribe(playChargeAnimation)
 
 -- Charge that baby up
 projectiles:flatMap(function (instance)
@@ -49,15 +105,7 @@ projectiles:flatMap(function (instance)
 				state.chargeTime.Value / config.chargeTime.Value,
 				inputUtil.getMouseHit()
 		end)
-end):subscribe(function (instance, chargeProportion, target)
-	local config = instance.config.projectile
-	local start = axisUtil.getPosition(instance)
-	local vx = math.clamp(chargeProportion, 0, 1)
-	local velocity = config.maxThrowVelocity.Value * vx + config.minThrowVelocity.Value * (1 - vx)
-	pickupUtil.stripObject(instance)
-	projectileUtil.fireOwnedProjectile(instance, start, target, velocity)
-	genes.projectile.net.ReleaseRequested:FireServer(instance, start, target, velocity)
-end)
+end):subscribe(throw)
 
 -- Get pickup stream
 pickupUtil.getActivatedStream(genes.projectile):subscribe(function (instance, input)
